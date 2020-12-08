26cd8f014d506c68dc33e1d0fac0b52b0f3af597
ravenusmc <mcuddy77@gmail.com>
2020-12-08 18:42:04 -0500

    added more to first algorithm

--- one.py
+++ one.py
@@ -1,7 +1,6 @@
  # Given an integer, write a function to print it out in words (e.g. given 342, print "three-hundred forty-two"
 
 def convert_hundreds_to_words(hundreds_value):
-	print(hundreds_value)
 	if hundreds_value == '1':
 		return 'one-hundred'
 	elif hundreds_value == '2':
@@ -12,6 +11,38 @@ def convert_hundreds_to_words(hundreds_value):
 		return 'four-hundred'
 	elif hundreds_value == '5':
 		return 'five-hundred'
+	elif hundreds_value == '6':
+		return 'six-hundred'
+	elif hundreds_value == '7':
+		return 'seven-hundred'
+	elif hundreds_value == '8':
+		return 'eight-hundred'
+	elif hundreds_value == '9':
+		return 'nine-hundred'
+
+def convert_tens_to_words(tens_value):
+	if tens_value) == '0':
+		return ' '
+	elif tens_value) == '1':
+		#Need to write another function here to hand the tens. 
+		return 'one-hundred'
+	elif tens_value) == '2':
+		return 'twenty'
+	elif tens_value) == '3':
+		return 'thirty'
+	elif tens_value) == '4':
+		return 'four-hundred'
+	elif tens_value) == '5':
+		return 'five-hundred'
+	elif tens_value) == '6':
+		return 'six-hundred'
+	elif tens_value) == '7':
+		return 'seven-hundred'
+	elif tens_value) == '8':
+		return 'eight-hundred'
+	elif tens_value) == '9':
+		return 'nine-hundred'
+
 
 	
 def convert_number(number):
@@ -20,9 +51,11 @@ def convert_number(number):
 	if length_of_string == 3:
 		words_list = []
 		hundreds_value = number_to_string[0]
+		tens_value = number_to_string[1]
 		hundreds_spot_to_words = convert_hundreds_to_words(hundreds_value)
 		words_list.append(hundreds_spot_to_words)
-		print(words_list)
+		tens_spot_to_words = convert_tens_to_words(tens_value)
+
 	
 convert_number(325)
 

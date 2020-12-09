495d4349bd3897c3005cac80d4dfaac58e1eb2c3
ravenusmc <mcuddy77@gmail.com>
2020-12-09 18:41:08 -0500

    finished first pass at algorithm

--- one.py
+++ one.py
@@ -2,23 +2,23 @@
 
 def convert_hundreds_to_words(hundreds_value):
 	if hundreds_value == '1':
-		return 'one-hundred'
+		return 'one-hundred '
 	elif hundreds_value == '2':
-		return 'two-hundred'
+		return 'two-hundred '
 	elif hundreds_value == '3':
-		return 'three-hundred'
+		return 'three-hundred '
 	elif hundreds_value == '4':
-		return 'four-hundred'
+		return 'four-hundred '
 	elif hundreds_value == '5':
-		return 'five-hundred'
+		return 'five-hundred '
 	elif hundreds_value == '6':
-		return 'six-hundred'
+		return 'six-hundred '
 	elif hundreds_value == '7':
-		return 'seven-hundred'
+		return 'seven-hundred '
 	elif hundreds_value == '8':
-		return 'eight-hundred'
+		return 'eight-hundred '
 	elif hundreds_value == '9':
-		return 'nine-hundred'
+		return 'nine-hundred '
 
 def convert_tens_to_words(tens_value):
 	if tens_value == '0':
@@ -27,21 +27,43 @@ def convert_tens_to_words(tens_value):
 		#Need to write another function here to hand the tens. 
 		return 'one-hundred'
 	elif tens_value == '2':
-		return 'twenty'
+		return 'twenty-'
 	elif tens_value == '3':
-		return 'thirty'
+		return 'thirty-'
 	elif tens_value == '4':
-		return 'fourty'
+		return 'fourty-'
 	elif tens_value == '5':
-		return 'fifty'
+		return 'fifty-'
 	elif tens_value == '6':
-		return 'sixty'
+		return 'sixty-'
 	elif tens_value == '7':
-		return 'seventy'
+		return 'seventy-'
 	elif tens_value == '8':
-		return 'eighty'
+		return 'eighty-'
 	elif tens_value == '9':
-		return 'ninety'
+		return 'ninety-'
+
+def convert_ones_to_words(ones_value):
+	if ones_value == '0':
+		return ' '
+	elif ones_value == '1':
+		return 'one'
+	elif ones_value == '2':
+		return 'two'
+	elif ones_value == '3':
+		return 'three'
+	elif ones_value == '4':
+		return 'four'
+	elif ones_value == '5':
+		return 'five'
+	elif ones_value == '6':
+		return 'six'
+	elif ones_value == '7':
+		return 'seven'
+	elif ones_value == '8':
+		return 'eight'
+	elif ones_value == '9':
+		return 'nine'
 
 
 def convert_number(number):
@@ -51,12 +73,16 @@ def convert_number(number):
 		words_list = []
 		hundreds_value = number_to_string[0]
 		tens_value = number_to_string[1]
+		ones_value = number_to_string[2]
 		hundreds_spot_to_words = convert_hundreds_to_words(hundreds_value)
 		words_list.append(hundreds_spot_to_words)
 		tens_spot_to_words = convert_tens_to_words(tens_value)
 		words_list.append(tens_spot_to_words)
-		print(tens_spot_to_words)
+		ones_spot_to_words = convert_ones_to_words(ones_value)
+		words_list.append(ones_spot_to_words)
+		final_word = ""
+		print(final_word.join(words_list))
 
 	
-convert_number(325)
+convert_number(342)
 

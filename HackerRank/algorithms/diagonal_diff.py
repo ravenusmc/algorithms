c3f983b3223e04e1b1435eb3854383b8aa01aaba
ravenusmc <mcuddy77@gmail.com>
2020-12-18 08:26:16 -0500

    working on algorithm challenge

--- HackerRank/algorithms/diagonal_diff.py
+++ HackerRank/algorithms/diagonal_diff.py
@@ -3,6 +3,16 @@
 # URL https://www.hackerrank.com/challenges/diagonal-difference/problem?h_r=next-challenge&h_v=zen
 # Rank: 1,670,172
 
+arr = [
+[1, 2, 3],
+[4, 5, 6],
+[9, 8, 9]
+]
 
-def diagonalDifference(arr):
+# I need following values:
+arr[0][0]
+print(arr[0][0])
+
+
+# def diagonalDifference(arr):
     # Write your code here

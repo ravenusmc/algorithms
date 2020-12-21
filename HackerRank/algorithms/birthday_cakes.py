bf214c39fce7d0f8b92c42f4241461f76e30c382
ravenusmc <mcuddy77@gmail.com>
2020-12-21 13:59:59 -0500

    completed another algorithm

---
+++ HackerRank/algorithms/birthday_cakes.py
@@ -0,0 +1,5 @@
+# You are in charge of the cake for a child's birthday. You have decided the cake will have one candle 
+# for each year of their total age. They will only be able to blow out the tallest of the candles. 
+# Count how many candles are tallest.
+# RANK: 1,250,688
+# URL: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
--- HackerRank/algorithms/mini-max-sum.py
+++ HackerRank/algorithms/mini-max-sum.py
@@ -4,6 +4,25 @@
 # URL: https://www.hackerrank.com/challenges/mini-max-sum/problem
 # Rank: 1,338,720
 
-arr = [1, 2, 3, 4, 5]
+# arr = [1, 2, 3, 4, 5]
+# arr = [1, 3, 5, 7, 9]
+arr = [769082435, 210437958, 673982045, 375809214, 380564127]
+#expected_output = 1640793344 2199437821
 
 def miniMaxSum(arr):
+	index = 0
+	max_sum = 0
+	min_sum = sum(arr)
+	while index < len(arr):
+		changed_list = arr.copy()
+		changed_list.pop(index)
+		temp_max_sum = sum(changed_list)
+		temp_min_sum = sum(changed_list)
+		if temp_max_sum > max_sum:
+			max_sum = temp_max_sum
+		if temp_min_sum < min_sum:
+			min_sum = temp_min_sum
+		index += 1 
+	print(min_sum, max_sum)
+
+miniMaxSum(arr)
